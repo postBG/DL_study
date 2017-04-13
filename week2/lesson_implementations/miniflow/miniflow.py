f2827f87d265582f10221f8fd63322c3002fc5ac
unknown <profile2697@gmail.com>
2017-04-13 22:54:37 +0900

    [week2] sigmoid function 추가

--- week2/lesson_implementations/miniflow/miniflow.py
+++ week2/lesson_implementations/miniflow/miniflow.py
@@ -81,6 +81,40 @@ class Linear(Node):
         self.value = np.dot(inputs, weights) + bias
 
 
+class Sigmoid(Node):
+    """
+    You need to fix the `_sigmoid` and `forward` methods.
+    """
+
+    def __init__(self, node):
+        Node.__init__(self, [node])
+
+    @staticmethod
+    def _sigmoid(x):
+        """
+        This method is separate from `forward` because it
+        will be used later with `backward` as well.
+
+        `x`: A numpy array-like object.
+
+        Return the result of the sigmoid function.
+
+        Your code here!
+        """
+        return 1 / (1 + np.exp(-x))
+
+    def forward(self):
+        """
+        Set the value of this node to the result of the
+        sigmoid function, `_sigmoid`.
+
+        Your code here!
+        """
+        # This is a dummy value to prevent numpy errors
+        # if you test without changing this method.
+        self.value = self._sigmoid(self.inbound_nodes[0].value)
+
+
 """
 No need to change anything below here!
 """
