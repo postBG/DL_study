d9d6fa6c5fca708a77740331c60a3b8f875bd8c7
unknown <profile2697@gmail.com>
2017-04-14 02:35:12 +0900

    [week2] SGC implemented

--- week2/lesson_implementations/miniflow/miniflow.py
+++ week2/lesson_implementations/miniflow/miniflow.py
@@ -314,3 +314,22 @@ def forward_and_backward(graph):
     # see: https://docs.python.org/2.3/whatsnew/section-slices.html
     for n in graph[::-1]:
         n.backward()
+
+
+def sgd_update(trainables, learning_rate=1e-2):
+    """
+    Updates the value of each trainable with SGD.
+
+    Arguments:
+
+        `trainables`: A list of `Input` Nodes representing weights/biases.
+        `learning_rate`: The learning rate.
+    """
+    # TODO: update all the `trainables` with SGD
+    # You can access and assign the value of a trainable with `value` attribute.
+    # Example:
+    # for t in trainables:
+    #   t.value = your implementation here
+    for t in trainables:
+        partial = t.gradients[t]
+        t.value -= learning_rate * partial
