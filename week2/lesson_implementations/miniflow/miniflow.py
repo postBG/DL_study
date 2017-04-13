2c1ed1541df0b077d55b7f0c5e576783a5272082
unknown <profile2697@gmail.com>
2017-04-13 23:15:01 +0900

    [week2] mse cost node implementation

--- week2/lesson_implementations/miniflow/miniflow.py
+++ week2/lesson_implementations/miniflow/miniflow.py
@@ -115,6 +115,34 @@ class Sigmoid(Node):
         self.value = self._sigmoid(self.inbound_nodes[0].value)
 
 
+class MSE(Node):
+    def __init__(self, y, a):
+        """
+        The mean squared error cost function.
+        Should be used as the last node for a network.
+        """
+        # Call the base class' constructor.
+        Node.__init__(self, [y, a])
+
+    def forward(self):
+        """
+        Calculates the mean squared error.
+        """
+        # NOTE: We reshape these to avoid possible matrix/vector broadcast
+        # errors.
+        #
+        # For example, if we subtract an array of shape (3,) from an array of shape
+        # (3,1) we get an array of shape(3,3) as the result when we want
+        # an array of shape (3,1) instead.
+        #
+        # Making both arrays (3,1) insures the result is (3,1) and does
+        # an elementwise subtraction as expected.
+        y = self.inbound_nodes[0].value.reshape(-1, 1)
+        a = self.inbound_nodes[1].value.reshape(-1, 1)
+        # TODO: your code here
+        self.value = np.mean(np.square(y - a))
+
+
 """
 No need to change anything below here!
 """
